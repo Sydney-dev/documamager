50376413b216ee1a7d667e89f3c910d97ad7be04
sydney.chauke <sydney.chauke@git.com>
2020-07-30 09:34:21 +0200

    Initial commit

---
+++ src/main/java/com/qdb/documan/DocumamagerApplication.java
@@ -0,0 +1,13 @@
+package com.qdb.documan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumamagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumamagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qdb/documan/DocumamagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdb.documan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumamagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
