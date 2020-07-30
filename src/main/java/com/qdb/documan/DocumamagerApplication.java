7ddf2738724b3742e4fc0aa3d008c4e219757a0d
sydney.chauke <sydney.chauke@git.com>
2020-07-30 10:11:11 +0200

    Configuring Open api

--- src/main/java/com/qdb/documan/DocumamagerApplication.java
+++ src/main/java/com/qdb/documan/DocumamagerApplication.java
@@ -1,13 +1,32 @@
 package com.qdb.documan;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class DocumamagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DocumamagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DocumamagerApplication.class, args);
+    }
 
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Document Service API")
+                        .version(appVersion)
+                        .description(appDesciption)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 }
